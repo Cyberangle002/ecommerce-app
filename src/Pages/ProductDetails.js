e09cc697311d42693c918a4e87c5e43dac88906f
Cyberangle002 <cyberchuk319031@gmail.com>
2025-11-02 12:14:39 +0530

    Fixed folder casing for CartContext imports

--- src/Pages/ProductDetails.js
+++ src/Pages/ProductDetails.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { useParams } from "react-router-dom";
-import { CartContext } from "../context/CartContext";
+import { CartContext } from "../Context/CartContext";
 import { motion } from "framer-motion";
 import "./ProductDetails.css";
 
